dbbad8f0ca71aede15d0b17d444245c53e124943
Arina <arina.smal@mail.ru>
2022-10-24 11:32:42 +0300

    mmm

---
+++ 18) swap.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+// передача по указателю
+void Swap(int* a, int* b) {
+    int temp = *a; // временная переменная 
+    *a = *b; // разыменование 
+    *b = temp;
+}
+
+int main() {
+    int a = 32;
+    int b = 45;
+
+    cout << "a = " << a << ' ' << "b = " << b << endl;
+
+    cout << "Swap" << endl;
+
+    Swap(&a, &b);
+
+    cout << "a = " << a << ' ' << "b = " << b << endl;
+}
+
+void swap2(int& a, int& b) {
+    a = a xor b;
+    b = b xor a;
+    a = a xor b;
+}
+int main() {
+    int a, b;
+    cout << "Enter two numbers" << endl;
+    cin >> a >> b;
+    swap2(a, b);
+    cout << a << " " << b << endl;
+
+    return 0;
+}
---
+++ 19) Sorting.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+void insertionSort(int list[], int listLength)
+{
+	for (int i = 1; i < listLength; i++)
+	{
+		int j = i - 1;
+		while (j >= 0 && list[j] > list[j + 1])
+		{
+			swap(list[j], list[j + 1]);
+			j--;
+		}
+	}
+}
+
+int main()
+{
+	int list[5] = { 4, 178, 93, -18, 6 };
+	cout << "Input array \n";
+	for (int i = 0; i < 5; i++)
+	{
+		cout << list[i] << "\t";
+	}
+
+	insertionSort(list, 5);
+
+	cout << "\n\nSorted array \n";
+	for (int i = 0; i < 5; i++)
+	{
+		cout << list[i] << "\t";
+	}
+
+	return 0;
+}
